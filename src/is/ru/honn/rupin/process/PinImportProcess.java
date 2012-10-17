234690f2dfc5374be9557562767944a51c83e455
Kristjan <kristjaneldjarn@google.com>
2012-10-17 00:44:22 +0000

    Create User and Board added to PinImportProcess.java

--- src/is/ru/honn/rupin/process/PinImportProcess.java
+++ src/is/ru/honn/rupin/process/PinImportProcess.java
@@ -4,18 +4,18 @@ import is.ru.honn.rupin.domain.Board;
 import is.ru.honn.rupin.domain.Pin;
 import is.ru.honn.rupin.domain.User;
 import is.ru.honn.rupin.feeds.FeedEntry;
-import is.ru.honn.rupin.feeds.FeedException;
 import is.ru.honn.rupin.feeds.FeedHandler;
 import is.ru.honn.rupin.feeds.FeedReader;
 import is.ru.honn.rupin.service.BoardNotFoundException;
 import is.ru.honn.rupin.service.PinService;
+import is.ru.honn.rupin.service.UserNotFoundException;
+import is.ru.honn.rupin.service.UsernameExistsException;
 import is.ruframework.process.RuAbstractProcess;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.MessageSource;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import java.util.List;
-import java.util.Locale;
 import java.util.logging.Logger;
 
 /**
@@ -26,27 +26,27 @@ import java.util.logging.Logger;
  */
 public class PinImportProcess extends RuAbstractProcess implements FeedHandler {
     //todo: skoða F11+Process+Design+A.avi 34:53mín
+
     Logger log = Logger.getLogger(this.getClass().getName());
     PinService pinService;
     FeedReader reader;
     MessageSource msg;
-    User user = new User();
-    Board board = new Board();
+    User user;// = new User();
+    Board board;// = new Board();
 
     public void beforeProcess() {
+        ApplicationContext psx = new FileSystemXmlApplicationContext("process.xml");
 
         ApplicationContext ctx = new FileSystemXmlApplicationContext("app.xml");
         pinService = (PinService) ctx.getBean("contentService");
         reader = (FeedReader) ctx.getBean("feedReader");
         reader.setFeedHandler(this);
         msg = (MessageSource) ctx.getBean("messageSource");
-        log.info(msg.getMessage("processbefore",
+    /*    log.info(msg.getMessage("processbefore",
                 new Object[]{getProcessContext().getProcessName()},
                 // Locale.ENGLISH));
-                Locale.getDefault()));
+                Locale.getDefault()));    */
 
-    /*
-        ApplicationContext psx = new FileSystemXmlApplicationContext("process.xml");
         user = (User) psx.getBean("userBean");
         try {
             pinService.signUpUser(user.getUsername(), user.getFirstName(), user.getLastName(),
@@ -55,11 +55,12 @@ public class PinImportProcess extends RuAbstractProcess implements FeedHandler {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
 
+        board = (Board) psx.getBean("boardBean");
         try {
             pinService.createBoard(user.getUsername(), board.getName(), board.getCategory());
         } catch (UserNotFoundException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }  */
+        }
 
 
     }
@@ -67,7 +68,7 @@ public class PinImportProcess extends RuAbstractProcess implements FeedHandler {
     public void processEntry(FeedEntry entry) {
         //todo: þetta á örugglega að vera eins og processContent fallið í verkefni vika06
         try {
-            pinService.createPin(user.getUsername(), board.getName(), board.getCategory(), "description");
+            pinService.createPin(user.getUsername(), board.getName(), "www.mbl.is", "description");
         } catch (BoardNotFoundException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
@@ -75,21 +76,15 @@ public class PinImportProcess extends RuAbstractProcess implements FeedHandler {
 
     @Override
     public void startProcess() {
-        log.info(msg.getMessage("processstart",
+     /*   log.info(msg.getMessage("processstart",
                 new Object[]{getProcessContext().getProcessName()},
-                Locale.getDefault()));
-        try {
-            //reader.read(getProcessContext().getImportURL());
-            reader.read();   //  ATH....
-        } catch (FeedException e) {
-            log.info(msg.getMessage("processreaderror",
-                    new Object[]{getProcessContext().getImportFile()},
-                    Locale.getDefault()));
-            log.info(e.getMessage());
-        }
-        log.info(msg.getMessage("processstartdone",
+                Locale.getDefault()));         */
+        //reader.read(getProcessContext().getImportURL());
+        reader.setSource(getProcessContext().getImportURL());//?????
+
+        /*  log.info(msg.getMessage("processstartdone",
                 new Object[]{pinService.getPinsOnBoard(user.getUsername(), board.getName()).size()},
-                Locale.getDefault()));
+                Locale.getDefault()));       */
     }
 
     public void afterProcess() {
